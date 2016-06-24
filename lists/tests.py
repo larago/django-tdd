bc8f2a3ca119b8d0c6e8286b0aaf1183833c6082
casterock <binglux@gmail.com>
2016-06-24 08:01:41 +0800

    update functional_tests with more function

--- lists/tests.py
+++ lists/tests.py
@@ -45,6 +45,16 @@ class HomePageTest(TestCase):
         home_page(request)
         self.assertEqual(Item.objects.count(), 0)
 
+    def test_home_page_display_all_list_items(self):
+        Item.objects.create(text='itemey 1')
+        Item.objects.create(text='itemey 2')
+
+        request = HttpRequest()
+        response = home_page(request)
+
+        self.assertIn('itemey 1', response.content.decode())
+        self.assertIn('itemey 2', response.content.decode())
+
 class ItemModelTest(TestCase):
 
     def test_saving_and_retrieving_items(self):
@@ -63,15 +73,3 @@ class ItemModelTest(TestCase):
         second_saved_item = saved_items[1]
         self.assertEqual(first_saved_item.text, 'The first (ever) list item')
         self.assertEqual(second_saved_item.text, 'Item the second')
-
-class HomePageTest(TestCase):
-
-    def test_home_page_display_all_list_items(self):
-        Item.objects.create(text='itemey 1')
-        Item.objects.create(text='itemey 2')
-
-        request = HttpRequest()
-        response = home_page(request)
-
-        self.assertIn('itemey 1', response.content.decode())
-        self.assertIn('itemey 2', response.content.decode())
